47067939db6415579fc256d88ece973ee5543006
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2023-12-27 11:10:06 +0530

    Singleton tests

---
+++ DesignPatterns.Test/Creational.Tests/SingletonTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DesignPatterns.Creational.SingletonPattern;
+
+namespace DesignPatterns.Test.Creational.Tests
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void Singleton_Constructor_ReturnsSingleObject()
+        {
+            // Arrange - Varriable, classes, Mock
+
+
+            // Act
+            Singleton singleton1 = Singleton.getSingleton();
+            Singleton singleton2 = Singleton.getSingleton();
+
+            //Assert
+            Assert.True(singleton1 == singleton2);
+            //Assert.NotEqual(singleton1, singleton2);
+        }
+    }
+}
---
+++ DesignPatterns.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DesignPatterns.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
