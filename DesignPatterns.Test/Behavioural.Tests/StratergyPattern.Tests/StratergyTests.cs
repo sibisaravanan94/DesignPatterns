aa39e3b63004f449c26a4b6507431396b7a7b69f
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2023-12-30 13:31:08 +0530

    Stratergy pattern tests

---
+++ DesignPatterns.Test/Behavioural.Tests/StratergyPattern.Tests/StratergyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.Behavioural.StratergyPattern;
+using Xunit;
+
+namespace DesignPatterns.Test.Behavioural.Tests.StratergyPattern.Tests
+{
+    public class StratergyTests
+    {
+        [Fact]
+        public void testStratergy()
+        {
+            Navigator car = new Navigator(new FourWheelerStratergy());
+            double time = car.navigate("Namakkal", "Salem");
+            Assert.Equal(time,(Double)(4.0));
+
+            Navigator bike = new Navigator(new TwoWheelerStratergy());
+            double bikeTime = bike.navigate("Namakkal", "Salem");
+            Assert.Equal(bikeTime, 2.0);
+        }
+    }
+}
